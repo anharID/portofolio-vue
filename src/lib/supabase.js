c50d6c7efd735796ba07796b6ad1023da198e3a8
Anhar <63109799+anharID@users.noreply.github.com>
2025-11-26 20:43:29 +0700

    update supabase

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.SUPABASE_DATABASE_URL
-const supabaseKey = import.meta.env.SUPABASE_ANON_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_DATABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
